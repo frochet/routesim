7b440709d5f9888e686ca8868130d92fe169a665
Florentin Rochet <florentin.rochet@gmail.com>
2021-08-20 18:29:11 +0100

    interface for random sampling done

--- src/config.rs
+++ src/config.rs
@@ -4,17 +4,22 @@ use std::io::BufReader;
 use std::io::BufRead;
 use std::path::Path;
 use crate::mixnodes::mixnode::Mixnode;
+use std::vec::IntoIter;
+use rand::prelude::*;
+use rand::distributions::WeightedIndex;
 
 /// A config is a set of hashmaps containing mixes
 #[derive(Default)]
 pub struct Config {
-    layers: [HashMap<u32, Mixnode>; 3],
-    unselected: HashMap<u32, Mixnode>
+    layers: [Vec<Mixnode>; 3],
+    wc_layers: [Box<Option<WeightedIndex<f64>>>; 3],
+    unselected: HashMap<u32, Mixnode>,
 }
 
 impl Config {
     pub fn new() -> Self {
         Config {
+            wc_layers: [Box::new(None), Box::new(None), Box::new(None)],
             ..Default::default()
         }
     }
@@ -23,13 +28,25 @@ impl Config {
     pub fn update(&self) {
     }
 
-    pub fn layers(&self) -> &[HashMap<u32, Mixnode>] {
+    pub fn layers(&self) -> &[Vec<Mixnode>] {
         &self.layers
     }
     pub fn unselected(&self) -> &HashMap<u32, Mixnode> {
         &self.unselected
     }
+    pub fn sample_path(&self, rng: &mut ThreadRng) -> IntoIter<u32> {
+        let mut path = vec![0 as u32; 3];
+        // TODO sample the path
+        // and returns an owned iterator
+        for i in 0..3 {
+            if let Some(wc) = &*self.wc_layers[i] {
+                path[i] = self.layers[i][wc.sample(rng)].mixid;
+            }
+        }
+        path.into_iter()
+    }
 }
+
 pub fn load<P>(filename: P) -> Config
 where
     P: AsRef<Path>,
@@ -39,15 +56,19 @@ where
     //skip header
     for line_r in BufReader::new(file).lines().skip(1) {
         if let Ok(line) = line_r {
-            let mix: Mixnode = line.parse().expect(&format!("Unable to parse into a Mixnode -- Is your data correct? {}", line));
+            let mix: Mixnode = line.parse().expect(&format!("Unable to parse into a Mixnode -- Is
+                                                            your data correct? {}", line));
             match mix.layer {
-                0 | 1 | 2 => config.layers[mix.layer as usize].insert(mix.mixid, mix),
-                _ => config.unselected.insert(mix.mixid, mix),
+                0 | 1 | 2 => config.layers[mix.layer as usize].push(mix),
+                _ => {config.unselected.insert(mix.mixid, mix);},
             };
         }
         else {
             println!("Something went wrong while reading");
         }
+    }
+    for i in 0..3 {
+        config.wc_layers[i] = Box::new(Some(WeightedIndex::new(config.layers[i].iter().map(|item| item.weight)).unwrap()));
     }
     config
 }
@@ -55,12 +76,16 @@ where
 #[test]
 fn load_test_config() {
     let config = load("testfiles/1000_137_Random_BP_layout.csv");
-    let mix = config.layers()[0].get(&42).unwrap();
+    let mix = &config.layers()[0][42];
     //42│20.430784458454426│False│0
     assert_eq!(mix.is_malicious, false);
-    assert_eq!(mix.weight,20.430784458454426);
+    assert_eq!(mix.weight,1.8480844586590168);
 }
-
-
-
-
+#[test]
+fn test_sample_path() {
+    let config = load("testfiles/1000_137_Random_BP_layout.csv");
+    let mut rng = thread_rng();
+    let path = config.sample_path(&mut rng);
+    let path = path.collect::<Vec<_>>();
+    assert_eq!(path.len(), 3);
+}
--- src/routesim.rs
+++ src/routesim.rs
@@ -1,5 +1,7 @@
 use config::Config;
-
+use usermodel::UserModel;
+use rayon::prelude::*;
+use rand::prelude::*;
 /// Contains information required for running the simulation
 pub struct Runable {
     /// The number of users we want to simulate
@@ -8,18 +10,36 @@ pub struct Runable {
     config: Config,
     /// The number of virtual days for running the experiment
     days: u32,
-    /// todo
-    //usermod: UserModel,
+
+    outputfilename: String,
 }
 
 impl Runable {
     pub fn new() -> Self {
         Runable { ..Default::default() }
     }
+
+    fn format_message_timing(timing: u64) -> String {
+        "".to_string()
+    }
+    fn days_to_timestamp(&self) {
+        self.days*24*60*60
+    }
     /// Run the simulation -- this function should output
     /// route taken for each user each time the user requires to send
     /// a message, which depends of the user model through time.
-    pub fn run(&self) {
+    pub fn run<T: UserModel+Iterator+Default>(&self) {
+        let days_in_timestamp = self.days_in_timestamp();
+
+        (0..self.users).par_iter().for_each(|user| {
+            let usermodel = T::new();
+            let mut rng = thread_rng();
+            usermodel.set_limit(self.days_in_timestamp(self.days));
+            for message_timing in self.usermodel.iter() {
+                let path = self.config.sample_path(&mut rng);
+                // write out the path for this message_timing
+            }
+        })
 
     }
 }
