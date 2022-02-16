e5721ed50804cac23c7ba1ddeebe61d97d39e19a
Florentin Rochet <frochet@ed.ac.uk>
2022-02-16 14:17:10 +0000

    add file layout

---
+++ testfiles/split_csv_file.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+FILE=$1
+FILENAME=$(basename -- "$FILE")
+count_cols() {
+	head -1 $FILE | sed 's/[^,]//g' | wc -c
+}
+
+
+for (( c=3 ; c<=$(count_cols); c++ ))
+do	
+	epoch=$((c-4))
+	cut -d "," -f 1-3,$c < $FILE > $PWD/epoch${epoch}_${FILENAME}
+done
